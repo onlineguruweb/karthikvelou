4ec6a2898325aaa19062b557d3748e10cd0b53aa
online guru web <44157539+onlineguruweb@users.noreply.github.com>
2022-09-24 19:43:03 +0530

    node update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 
-app.get("/",(req,res)=>{ res.send("I am noe js server");});
+app.get("/",(req,res)=>{ res.send("I am node js server");});
 app.listen(4000,()=>{console.log("I am listening 4000");});
